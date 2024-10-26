722845e0a2251671c6bd52a30be6448e4727150e
M450NCH00 <99965994+M450NCH00@users.noreply.github.com>
2024-10-27 00:06:43 +0800

    Add files via upload

---
+++ SC2002/src/mainSystemControl/MainCtrl.java
@@ -0,0 +1,35 @@
+package mainSystemControl;
+
+import ShowUserMenu.*;
+import userInfo.User;
+import userInfoControl.GetOperationInput;
+import userInfoControl.MedicalRecordCtrl;
+import userInfoControl.PatientCtrl;
+
+public class MainCtrl {
+	private User user;
+	private PatientCtrl patientCtrl = null;
+	private MedicalRecordCtrl medicalRecordCtrl; 
+	private GetOperationInput operationInput;
+	ShowMenu showMenu = null;
+	
+	public MainCtrl(String inputRole, String hospitalID, String name) {
+		user = new User(Role.valueOf(inputRole), hospitalID, name);
+		switch (user.getRole()) {
+		case Role.PATIENT: 
+			this.patientCtrl = new PatientCtrl(user.getHospitalId());
+			this.medicalRecordCtrl = this.patientCtrl;
+			this.operationInput = this.patientCtrl;
+			this.showMenu = new ShowPatientMenu();
+			break;
+		}
+	}
+	
+	public void showMenu() {
+		showMenu.showMenu();
+	}
+	
+	public void getOperationInput(int input) {
+		operationInput.getOperationInput(input);
+	}
+}
