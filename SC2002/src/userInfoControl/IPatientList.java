73a13bea0dbb7ee4b81ec8307c33ab293c54175c
KehJingXiang <kehjingxiang@yahoo.com>
2024-11-17 17:08:03 +0800

    updated javadoc

--- SC2002/src/userInfoControl/IPatientList.java
+++ SC2002/src/userInfoControl/IPatientList.java
@@ -1,6 +1,17 @@
 package userInfoControl;
 
+/**
+ * The IPatientList interface provides a method for viewing patient IDs.
+ * This interface is designed to be implemented by classes that handle 
+ * patient-related functionalities, particularly the retrieval and display 
+ * of patient identification details.
+ */
 public interface IPatientList {
 
+	/**
+     * Displays the list of patient IDs.
+     * Implementing classes should provide a concrete implementation for 
+     * how patient IDs are retrieved and displayed.
+     */
 	public abstract void viewPatientIDs();
 }
